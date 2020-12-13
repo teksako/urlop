f5dd3b78188f26b8a9b7609763a626632203ecf8
teksako <teksako@op.pl>
2020-12-13 10:02:26 +0100

    corrected uerDto

--- src/main/java/pl/sda/urlopy/dto/UserDto.java
+++ src/main/java/pl/sda/urlopy/dto/UserDto.java
@@ -27,43 +27,4 @@ public class UserDto {
 //    private Data createData;
 //    private AccountStatus status;
 
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getMatchingPassword() {
-        return matchingPassword;
-    }
-
-    public void setMatchingPassword(String matchingPassword) {
-        this.matchingPassword = matchingPassword;
-    }
 }
